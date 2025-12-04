739dbc1147e2f513e698068d44b3f2d484781bf3
Bright Kofi Dompeh <kekelidompeh@gmail.com>
2025-12-04 07:34:33 +0000

    codebase structure

---
+++ src/main/java/org/example/utils/ConsoleMenu.java
@@ -0,0 +1,14 @@
+package org.example.utils;
+
+public class ConsoleMenu {
+   private int input;
+   private bool exit;
+
+   public void setInput(int input) {
+
+   }
+
+   public void setExit(boolean exit) {
+
+   }
+}
